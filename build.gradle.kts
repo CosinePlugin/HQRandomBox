62503bc19a6874c9e378654215d599972a6706a9
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-10-11 21:57:06 +0900

    feat: 왼손에서 랜덤박스 사용 가능하게 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "kr.cosine.randombox"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
     maven("https://maven.hqservice.kr/repository/maven-public")
--- src/main/kotlin/kr/cosine/randombox/listener/RandomBoxListener.kt
+++ src/main/kotlin/kr/cosine/randombox/listener/RandomBoxListener.kt
@@ -4,7 +4,6 @@ import kr.cosine.randombox.view.RandomBoxService
 import kr.hqservice.framework.bukkit.core.listener.Listener
 import kr.hqservice.framework.bukkit.core.listener.Subscribe
 import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.inventory.EquipmentSlot
 
 @Listener
 class RandomBoxListener(
@@ -12,7 +11,6 @@ class RandomBoxListener(
 ) {
     @Subscribe
     fun onPlayerInteract(event: PlayerInteractEvent) {
-        if (event.hand != EquipmentSlot.HAND) return
         if (!event.action.name.contains("RIGHT_CLICK")) return
         val itemStack = event.item ?: return
         if (randomBoxService.useRandomBox(event.player, itemStack)) {
